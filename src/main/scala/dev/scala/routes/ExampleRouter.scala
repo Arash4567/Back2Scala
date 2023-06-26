4b18141a59cac3d13852e38f2003c53a179fcfd9
OG'ABEK <shomoyxhacker007@mail.ru>
2023-06-26 13:31:02 +0300

    feat: router samples added

--- src/main/scala/dev/scala/routes/ExampleRouter.scala
+++ src/main/scala/dev/scala/routes/ExampleRouter.scala
@@ -8,10 +8,20 @@ import org.http4s.server.Router
 import org.typelevel.log4cats.Logger
 
 class ExampleRouter[F[_]: Monad](implicit logger: Logger[F]) extends Http4sDsl[F] {
+  private object CountryQueryParamMatcher extends QueryParamDecoderMatcher[String]("country")
+
   private val publicRouter = HttpRoutes.of[F] {
-    case req @ GET -> Root / "status" :? aa =>
-      logger.info("Ura!") *>
-        Ok("Ok")
+    case req @ GET -> Root / "status" :? CountryQueryParamMatcher(country) =>
+      logger.info(s"Ura! - $country") *>
+        Ok(s"Ok - $country")
+
+    case req @ GET -> Root / "ping" =>
+      logger.info(s"Ura!") *>
+        Ok(s"Ok")
+
+    case req @ GET -> Root / "status" / "start" =>
+      logger.info(s"Ura!") *>
+        Ok(s"Ok - start")
   }
   val routes: HttpRoutes[F] = Router(
     "api/v1" -> publicRouter
